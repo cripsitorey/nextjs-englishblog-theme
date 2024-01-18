b055f5235b297dfa65e4a586741ee4dabeb380d4
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2024-01-18 11:30:56 -0500

    Update1

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Emilio Cuenca';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'English Blog: Teenagers in home';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : "A Emilio's production";
 
   return {
     name,
